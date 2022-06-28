3f2af4285e0e1ce87806dc25ef15863c1fd8decc
James R <jimmer1609@gmail.com>
2022-06-28 16:20:25 +0900

    mvp met

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [todos, setTodos] = useState([
+    {name: "Buy shopping", priority: "high"}, 
+    {name: "Clean bathroom", priority: "high"},
+    {name: "Car's MOT", priority: "high"},
+    {name: "Mow The Grass", priority: "high"}  
+  ])
+
+  const [newToDo, setNewToDo] = useState('');
+
+  const saveNewToDo = (evt) => {
+    evt.preventDefault()
+    const copyTodos = [...todos];
+    copyTodos.push(newToDo);
+    setTodos(copyTodos);
+    setNewToDo('');
+  }
+
+  const handleToDoInput = (evt) => {
+    setNewToDo(evt.target.value);
+  }
+
+  const todoNodes = todos.map((todo, index) => {
+    return (
+      <li key={index}>
+        <span>{todo.name}</span>
+      </li>
+    )
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Mt ToDo List</h1>
+      <hr></hr>
+
+      <ul>
+        {todoNodes}
+      </ul>
+
+      <form onSubmit={saveNewToDo}>
+
+        <label htmlFor='new-todo'>Add a new ToDo:</label>
+        <input id='new-todo' type='text' value={newToDo} onChange={handleToDoInput}/>
+
+        <label htmlFor='high-priority'>High</label>
+        <input id='high-priority' type='radio' value={newToDo} onChange={handleToDoInput}/>
+
+        <label htmlFor='low-priority'>Low</label>
+        <input id='low-priority' type='radio' value={newToDo} onChange={handleToDoInput}/>
+
+
+        <input type='submit' value='Add Task'/>
+      </form>
+
     </div>
   );
 }
